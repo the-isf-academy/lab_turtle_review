a10cdf951da72e803f6140b3d4c843c6adcca15a
emma <eqbrown@isf.edu.hk>
2025-08-14 14:10:04 +0800

    add function and rename shell

--- turtle_drawing.py
+++ turtle_drawing.py
@@ -1,11 +1,19 @@
 from turtle import *
 
 
-forward(50)
-right(90)
-forward(50)
-right(90)
-forward(50)
-right(90)
-forward(50)
-right(90)
+# define function with size parameter
+#   - how can you incorporate a for loop? 
+#   - how can you incorporate a color parameter? 
+def square(size):
+    forward(50)
+    right(90)
+    forward(50)
+    right(90)
+    forward(50)
+    right(90)
+    forward(50)
+    right(90)
+
+square(100) # calls function
+
+input() # keeps window open
